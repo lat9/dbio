6608c54a8519efbc768cec4f2e106417a01cdf7f
lat9 <lat9@vinosdefrutastropicales.com>
2016-02-24 09:14:13 -0500

    #16: Add Orders filters to OrdersProducts
    Re-use the filters defined by the Orders report for the OrdersProducts
    one.  This will also enable those filters for OrdersProductsAttribs
    since that report extends OrdersProducts.

--- includes/classes/dbio/DbIoOrdersProductsHandler.php
+++ includes/classes/dbio/DbIoOrdersProductsHandler.php
@@ -1,6 +1,6 @@
 <?php
 // -----
-// Part of the DataBase Import/Export (aka dbIO) plugin, created by Cindy Merkin (cindy@vinosdefrutastropicales.com)
+// Part of the DataBase I/O Manager (aka dbIO) plugin, created by Cindy Merkin (cindy@vinosdefrutastropicales.com)
 // Copyright (c) 2016, Vinos de Frutas Tropicales.
 //
 if (!defined ('IS_ADMIN_FLAG')) {
@@ -43,6 +43,45 @@ class DbIoOrdersProductsHandler extends DbIoHandler
             }
         }
         return $fields;
+    }
+    
+    
+    // -----
+    // This function gives the current handler the last opportunity to modify the SQL query clauses used for the current export.  It's
+    // usually provided by handlers that use an "export_filter", allowing the handler to inspect any filter-variables provided by
+    // the caller.
+    //
+    // Returns a boolean (true/false) indication of whether the export's initialization was successful.  If unsuccessful, the handler
+    // is **assumed** to have set its reason into the class message variable.
+    //
+    public function exportFinalizeInitialization ()
+    {
+        // -----
+        // Check to see if any of this handler's filter variables have been set.  If set, check the values and then
+        // update the where_clause for the to-be-issued SQL query for the export.
+        //
+        if ($_POST['orders_status'] != '0') {
+            $this->where_clause .= (($this->where_clause == '') ? '' : ' AND ') . 'orders_status = ' . (int)$_POST['orders_status'];
+        }
+        if (zen_not_null ($_POST['orders_id_min']) && ctype_digit ($_POST['orders_id_min'])) {
+            $this->where_clause .= (($this->where_clause == '') ? '' : ' AND ') . 'orders_id >= ' . (int)$_POST['orders_id_min'];
+        }
+        if (zen_not_null ($_POST['orders_id_max']) && ctype_digit ($_POST['orders_id_max'])) {
+            $this->where_clause .= (($this->where_clause == '') ? '' : ' AND ') . 'orders_id <= ' . (int)$_POST['orders_id_max'];
+        }
+        if (zen_not_null ($_POST['orders_date_start'])) {
+           $validated_date = dbioFormatValidateDate ($_POST['orders_date_start']);
+            if ($validated_date !== false) {
+                $this->where_clause .= (($this->where_clause == '') ? '' : ' AND ') . "date_purchased >= '$validated_date 00:00:00'";
+            }
+        }
+        if (zen_not_null ($_POST['orders_date_end'])) {
+            $validated_date = dbioFormatValidateDate ($_POST['orders_date_end']);
+            if ($validated_date !== false) {
+                $this->where_clause .= (($this->where_clause == '') ? '' : ' AND ') . "date_purchased <= '$validated_date 23:59:59'";
+            }
+        }
+        return true;
     }
     
 // ----------------------------------------------------------------------------------
