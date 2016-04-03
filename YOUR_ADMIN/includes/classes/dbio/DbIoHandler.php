b75e7e16b92c0fd949f0610a68fb6b324e970619
lat9 <lat9@vinosdefrutastropicales.com>
2016-04-03 16:25:17 -0400

    #39: Enable language-file movement
    Use the DbIo-specific constant to access all handlers' language files.

--- YOUR_ADMIN/includes/classes/dbio/DbIoHandler.php
+++ YOUR_ADMIN/includes/classes/dbio/DbIoHandler.php
@@ -797,7 +797,7 @@ abstract class DbIoHandler extends base
     
     protected final static function loadHandlerMessageFile ($handler_name)
     {
-        include_once (DIR_FS_ADMIN . DIR_WS_LANGUAGES . $_SESSION['language'] . '/dbio/DbIo' . $handler_name . 'Handler.php');
+        include_once (DIR_FS_DBIO_LANGUAGES . $_SESSION['language'] . '/dbio/DbIo' . $handler_name . 'Handler.php');
     }
     
     // -----
