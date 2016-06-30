b17d75694cd7aaf8a86731cb0fdbdc9680f69272
lat9 <lat9@vinosdefrutastropicales.com>
2016-06-30 16:11:49 -0400

    Properly BiCapitalize the plugin's name
    Update the Configuration settings label to use DbIo instead of dbIO.

--- YOUR_ADMIN/includes/languages/english/extra_definitions/dbio_admin_extra_definitions.php
+++ YOUR_ADMIN/includes/languages/english/extra_definitions/dbio_admin_extra_definitions.php
@@ -1,7 +1,7 @@
 <?php
 // -----
-// Part of the DataBase Import/Export (aka dbIO) plugin, created by Cindy Merkin (cindy@vinosdefrutastropicales.com)
+// Part of the DataBase Import/Export (aka DbiO) plugin, created by Cindy Merkin (cindy@vinosdefrutastropicales.com)
 // Copyright (c) 2016, Vinos de Frutas Tropicales.
 //
 define ('BOX_TOOLS_DBIO', 'Database I/O Manager');
-define ('BOX_CONFIGURATION_DBIO', 'dbIO Manager Settings');
+define ('BOX_CONFIGURATION_DBIO', 'DbIo Manager Settings');
