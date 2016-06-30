c1c56f41983673e82c4df1c7f620b9bfdc5f9f17
lat9 <lat9@vinosdefrutastropicales.com>
2016-06-30 16:14:47 -0400

    Update version to 0.0.10
    Update the initialization script to identify this version.

--- YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
@@ -7,8 +7,8 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('DBIO_CURRENT_VERSION', '0.0.9');
-define('DBIO_CURRENT_UPDATE_DATE', '2016-05-01');
+define('DBIO_CURRENT_VERSION', '0.0.10');
+define('DBIO_CURRENT_UPDATE_DATE', '2016-06-30');
 
 $version_release_date = DBIO_CURRENT_VERSION . ' (' . DBIO_CURRENT_UPDATE_DATE . ')';
 
