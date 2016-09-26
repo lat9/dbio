f7627f54fa57614760f32b2e2a87592c4ddbcc3f
lat9 <lat9@vinosdefrutastropicales.com>
2016-09-26 11:08:04 -0400

    #60: Missing language file
    Include the previously-missing file.

---
+++ YOUR_ADMIN/includes/languages/english/dbio/DbIoCustomersHandler.php
@@ -0,0 +1,10 @@
+<?php
+// -----
+// Part of the DataBase I/O Manager (aka DbIo) plugin, created by Cindy Merkin (cindy@vinosdefrutastropicales.com)
+// Copyright (c) 2016, Vinos de Frutas Tropicales.
+//
+
+// -----
+// Defines the handler's descriptive text.
+//
+define ('DBIO_CUSTOMERS_DESCRIPTION', 'This report-format supports the <em>export-only</em> of all fields within the &quot;customers&quot; table.');
