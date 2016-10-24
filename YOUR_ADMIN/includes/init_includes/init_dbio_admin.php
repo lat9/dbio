d482532a788f7b75938c9f77567e3797a52b2fc3
lat9 <lat9@vinosdefrutastropicales.com>
2016-10-24 13:12:20 -0400

    #66: Readme/configuration version/date update
    Update the release date/version for the plugin.

--- YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
@@ -7,8 +7,8 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('DBIO_CURRENT_VERSION', '1.0.1-beta2');
-define('DBIO_CURRENT_UPDATE_DATE', '2016-10-xx');
+define('DBIO_CURRENT_VERSION', '1.0.1');
+define('DBIO_CURRENT_UPDATE_DATE', '2016-10-24');
 
 $version_release_date = DBIO_CURRENT_VERSION . ' (' . DBIO_CURRENT_UPDATE_DATE . ')';
 
