8c937893a4a807ba1269f1f2a57f12f6cd67acdf
lat9 <lat9@vinosdefrutastropicales.com>
2016-12-19 15:05:35 -0500

    #77: Update version to 1.1.0
    Update the version in the readme and the database configuration to
    reflect v1.1.0.

--- YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
@@ -7,8 +7,8 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('DBIO_CURRENT_VERSION', '1.1.0-beta1');
-define('DBIO_CURRENT_UPDATE_DATE', '2016-12-xx');
+define('DBIO_CURRENT_VERSION', '1.1.0');
+define('DBIO_CURRENT_UPDATE_DATE', '2016-12-19');
 
 $version_release_date = DBIO_CURRENT_VERSION . ' (' . DBIO_CURRENT_UPDATE_DATE . ')';
 
