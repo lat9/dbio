a54e8484ceb6a6313f7f6f78ad76988abe8c65a9
lat9 <lat9@vinosdefrutastropicales.com>
2017-01-30 12:35:44 -0500

    #59: Custom exports, plugin uninstall sql
    Update the plugin's uninstall SQL, also removing the settings now added
    for the report customizations.

--- docs/dbio/uninstall/uninstall_dbio.sql
+++ docs/dbio/uninstall/uninstall_dbio.sql
@@ -1,4 +1,6 @@
 DELETE FROM configuration WHERE configuration_key LIKE 'DBIO_%';
 DELETE FROM configuration_group WHERE configuration_group_title = 'Database I/O Manager Settings' LIMIT 1;
 DROP TABLE IF EXISTS dbio_stats;
-DELETE FROM admin_pages WHERE page_key IN ('toolsDbIo', 'configDbIo');
+DROP TABLE IF EXISTS dbio_reports;
+DROP TABLE IF EXISTS dbio_reports_description;
+DELETE FROM admin_pages WHERE page_key IN ('toolsDbIo', 'configDbIo', 'toolsDbIoCustomize');
