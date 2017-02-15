e09c7431e24f844a4e521cc0fb122fa6aff84c85
lat9 <lat9@vinosdefrutastropicales.com>
2017-02-15 09:06:16 -0500

    #91, #92: Update configuration version
    Update the configuration version to reflect the repository's beta
    status.

--- YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
@@ -7,8 +7,8 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('DBIO_CURRENT_VERSION', '1.2.0');
-define('DBIO_CURRENT_UPDATE_DATE', '2017-02-06');
+define('DBIO_CURRENT_VERSION', '1.2.1-beta1');
+define('DBIO_CURRENT_UPDATE_DATE', '2017-02-xx');
 
 $version_release_date = DBIO_CURRENT_VERSION . ' (' . DBIO_CURRENT_UPDATE_DATE . ')';
 
