ba17050a8c7fd1e0e1a841bfba8cfa49b5083028
lat9 <lat9@vinosdefrutastropicales.com>
2017-03-01 08:16:30 -0500

    #91: Update readme/config versions
    Update the version/release date in the readme and configuration
    initialization.

--- YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
@@ -7,8 +7,8 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('DBIO_CURRENT_VERSION', '1.2.1-beta2');
-define('DBIO_CURRENT_UPDATE_DATE', '2017-02-xx');
+define('DBIO_CURRENT_VERSION', '1.2.1');
+define('DBIO_CURRENT_UPDATE_DATE', '2017-03-01');
 
 $version_release_date = DBIO_CURRENT_VERSION . ' (' . DBIO_CURRENT_UPDATE_DATE . ')';
 
