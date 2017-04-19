f034f5bc682855e2b154da18ff0e1c45ac7376cf
lat9 <lat9@vinosdefrutastropicales.com>
2017-04-19 08:54:45 -0400

    #99, #98: Initialization updates
    Correct the "early adopter" issue and add a configuration setting to
    allow store owners to control whether or not any category is
    auto-created during a product's import.

--- YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
@@ -7,8 +7,8 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('DBIO_CURRENT_VERSION', '1.2.1');
-define('DBIO_CURRENT_UPDATE_DATE', '2017-03-01');
+define('DBIO_CURRENT_VERSION', '1.3.0-beta1');
+define('DBIO_CURRENT_UPDATE_DATE', '2017-04-xx');
 
 $version_release_date = DBIO_CURRENT_VERSION . ' (' . DBIO_CURRENT_UPDATE_DATE . ')';
 
@@ -34,7 +34,11 @@ if ($configuration->EOF) {
 // ----
 // Record the configuration's current version in the database.
 //
-if (!defined ('DBIO_MODULE_VERSION')) {
+if (defined ('DBIO_MODULE_VERSION')) {
+    $dbio_versions = explode(' ', DBIO_MODULE_VERSION);
+    $dbio_current_version = $dbio_versions[0];
+} else {
+    $dbio_current_version = '0.0.0';
     $db->Execute("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, set_function) VALUES ('Version/Release Date', 'DBIO_MODULE_VERSION', '" . $version_release_date . "', 'The Database I/O Manager (DbIo) version number and release date.', $cgi, 1, now(), 'trim(')");
  
     $db->Execute ("INSERT INTO " . TABLE_CONFIGURATION . " ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) VALUES ( 'CSV: Delimiter', 'DBIO_CSV_DELIMITER', ',', 'Enter the single character that is used to separate columns within any DbIo CSV file.  To use the tab-character as the delimiter value, enter the word <b>TAB</b>.  (Default: <b>,</b>)', $cgi, 5, now(), NULL, NULL)");
@@ -59,124 +63,136 @@ if (!defined ('DBIO_MODULE_VERSION')) {
 }
 
 // -----
-// Update the configuration table to reflect the current version, if it's not already set.
+// If the plugin's version has changed, see if there's any additional configuration settings to be set.
 //
-if (!defined ('DBIO_MODULE_VERSION')) {
-    define ('DBIO_MODULE_VERSION', '0.0.0');
-} elseif (DBIO_MODULE_VERSION != $version_release_date) {
-    $db->Execute ("UPDATE " . TABLE_CONFIGURATION . " SET configuration_value = '" . $version_release_date . "' WHERE configuration_key = 'DBIO_MODULE_VERSION' LIMIT 1");
-}
+if (DBIO_MODULE_VERSION != $version_release_date) {
+    // -----
+    // Plugin version-specific updates ...
+    //
+    if (version_compare ($dbio_module_version, '1.1.0', '<')) {
+        $db->Execute ("INSERT IGNORE INTO " . TABLE_CONFIGURATION . " ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) VALUES ( '<em>Products</em> Import:  Allow Duplicate Models?', 'DBIO_PRODUCTS_ALLOW_DUPLICATE_MODELS', 'No', 'When performing a <em>Products</em> import, should an imported record be allowed if it would create a product with a duplicated model number?  (Default: <b>No</b>)', $cgi, 100, now(), NULL, 'zen_cfg_select_option(array(\'Yes\', \'No\'),')");
+    }
 
-// -----
-// Plugin version-specific updates ...
-//
-if (version_compare (DBIO_MODULE_VERSION, '1.1.0', '<')) {
-    $db->Execute ("INSERT IGNORE INTO " . TABLE_CONFIGURATION . " ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) VALUES ( '<em>Products</em> Import:  Allow Duplicate Models?', 'DBIO_PRODUCTS_ALLOW_DUPLICATE_MODELS', 'No', 'When performing a <em>Products</em> import, should an imported record be allowed if it would create a product with a duplicated model number?  (Default: <b>No</b>)', $cgi, 100, now(), NULL, 'zen_cfg_select_option(array(\'Yes\', \'No\'),')");
-}
-
-if (version_compare (DBIO_MODULE_VERSION, '1.2.0', '<')) {
-    if (!$sniffer->table_exists (TABLE_DBIO_REPORTS)) {
-        $db->Execute (
-            "CREATE TABLE " . TABLE_DBIO_REPORTS . " (
-                dbio_reports_id int(11) NOT NULL auto_increment,
-                handler_name varchar(255) NOT NULL,
-                report_name varchar(32) NOT NULL,
-                admin_id int(11) NOT NULL default '0',
-                last_updated_by int(11) NOT NULL default '0',
-                last_updated datetime default '0001-01-01 00:00:00',
-                field_info mediumblob,
-                PRIMARY KEY (dbio_reports_id),
-                KEY idx_dbio_handler_name (handler_name),
-                KEY idx_dbio_admin_id (admin_id)
-             ) ENGINE=MyISAM"
-        );
-        $db->Execute (
-            "CREATE TABLE " . TABLE_DBIO_REPORTS_DESCRIPTION . " (
-                dbio_reports_id int(11) NOT NULL,
-                language_id int(11) NOT NULL default '1',
-                report_description text,
-                PRIMARY KEY (dbio_reports_id,language_id)
-             ) ENGINE=MyISAM"
-        );
+    if (version_compare ($dbio_module_version, '1.2.0', '<')) {
+        if (!$sniffer->table_exists (TABLE_DBIO_REPORTS) || !$sniffer->field_exists (TABLE_DBIO_REPORTS, 'report_name')) {
+            $db->Execute (
+                "DROP TABLE IF EXISTS " . TABLE_DBIO_REPORTS . ", " . TABLE_DBIO_REPORTS_DESCRIPTION
+            );
+            $db->Execute (
+                "CREATE TABLE " . TABLE_DBIO_REPORTS . " (
+                    dbio_reports_id int(11) NOT NULL auto_increment,
+                    handler_name varchar(255) NOT NULL,
+                    report_name varchar(32) NOT NULL,
+                    admin_id int(11) NOT NULL default '0',
+                    last_updated_by int(11) NOT NULL default '0',
+                    last_updated datetime default '0001-01-01 00:00:00',
+                    field_info mediumblob,
+                    PRIMARY KEY (dbio_reports_id),
+                    KEY idx_dbio_handler_name (handler_name),
+                    KEY idx_dbio_admin_id (admin_id)
+                 ) ENGINE=MyISAM"
+            );
+            $db->Execute (
+                "CREATE TABLE " . TABLE_DBIO_REPORTS_DESCRIPTION . " (
+                    dbio_reports_id int(11) NOT NULL,
+                    language_id int(11) NOT NULL default '1',
+                    report_description text,
+                    PRIMARY KEY (dbio_reports_id,language_id)
+                 ) ENGINE=MyISAM"
+            );
+        }
+        
+        // -----
+        // If not already present, insert a couple of system-generated examples into the reports tables.
+        //
+        $install_check = $db->Execute ("SELECT * FROM " . TABLE_DBIO_REPORTS . " WHERE report_name IN ('quantity_only', 'meta_tags')");
+        if ($install_check->EOF) {
+            $languages = zen_get_languages ();
+            
+            $db->Execute (
+                "INSERT INTO " . TABLE_DBIO_REPORTS . "
+                    (handler_name, report_name, admin_id, last_updated_by, last_updated, field_info) 
+                VALUES
+                    ('Products', 'quantity_only', 0, 0, now(), 0x5b2270726f64756374735f6964222c2270726f64756374735f6d6f64656c222c2270726f64756374735f7175616e74697479225d)"
+            );
+            $dbio_reports_id = $db->Insert_ID ();
+            foreach ($languages as $current_language) {
+                $current_language_id = $current_language['id'];
+                $db->Execute (
+                    "INSERT INTO " . TABLE_DBIO_REPORTS_DESCRIPTION . "
+                        (dbio_reports_id, language_id, report_description) 
+                    VALUES
+                        ($dbio_reports_id, $current_language_id, 'This template supports products'' quantity updates, creating an exported file that contains a product''s ID, model-number and current quantity.')"
+                );
+            }
+            
+            $db->Execute (
+                "INSERT INTO " . TABLE_DBIO_REPORTS . "
+                    (handler_name, report_name, admin_id, last_updated_by, last_updated, field_info) 
+                VALUES
+                    ('Products', 'meta_tags', 0, 0, now(), 0x5b2270726f64756374735f6964222c2270726f64756374735f6d6f64656c222c226d657461746167735f7469746c655f737461747573222c226d657461746167735f70726f64756374735f6e616d655f737461747573222c226d657461746167735f6d6f64656c5f737461747573222c226d657461746167735f70726963655f737461747573222c226d657461746167735f7469746c655f7461676c696e655f737461747573225d)"
+            );
+            $dbio_reports_id = $db->Insert_ID ();
+            foreach ($languages as $current_language) {
+                $current_language_id = $current_language['id'];
+                $db->Execute (
+                    "INSERT INTO " . TABLE_DBIO_REPORTS_DESCRIPTION . "
+                        (dbio_reports_id, language_id, report_description) 
+                    VALUES
+                        ($dbio_reports_id, $current_language_id, 'This template supports the export of a product''s meta-tags, including each product''s ID, model-number and meta-tag-related fields.')"
+                );
+            }
+        }
     }
     
-    // -----
-    // If not already present, insert a couple of system-generated examples into the reports tables.
+    if (version_compare($dbio_module_version, '1.3.0', '<')) {
+        $db->Execute(
+            "INSERT IGNORE INTO " . TABLE_CONFIGURATION . " 
+                ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
+            VALUES 
+                ( '<em>Products</em>: Auto-Create Categories on Import?', 'DBIO_PRODUCTS_AUTO_CREATE_CATEGORIES', 'No', 'How should the <em>DbIo</em> handle missing categories on a <em>Products</em> import?  Choose <b>Yes</b> to have any missing categories automatially generated; choose <b>No</b> (the default) to disallow any product imports when the categories don\'t previously exist.', $cgi, 150, now(), NULL, 'zen_cfg_select_option(array(\'Yes\', \'No\'),')");
+    }
+
+    // ----
+    // Create the database tables for the I/O processing.
     //
-    $install_check = $db->Execute ("SELECT * FROM " . TABLE_DBIO_REPORTS . " WHERE report_name IN ('quantity_only', 'meta_tags')");
-    if ($install_check->EOF) {
-        $languages = zen_get_languages ();
-        
-        $db->Execute (
-            "INSERT INTO " . TABLE_DBIO_REPORTS . "
-                (handler_name, report_name, admin_id, last_updated_by, last_updated, field_info) 
-            VALUES
-                ('Products', 'quantity_only', 0, 0, now(), 0x5b2270726f64756374735f6964222c2270726f64756374735f6d6f64656c222c2270726f64756374735f7175616e74697479225d)"
-        );
-        $dbio_reports_id = $db->Insert_ID ();
-        foreach ($languages as $current_language) {
-            $current_language_id = $current_language['id'];
-            $db->Execute (
-                "INSERT INTO " . TABLE_DBIO_REPORTS_DESCRIPTION . "
-                    (dbio_reports_id, language_id, report_description) 
-                VALUES
-                    ($dbio_reports_id, $current_language_id, 'This template supports products'' quantity updates, creating an exported file that contains a product''s ID, model-number and current quantity.')"
-            );
+    $sql = "CREATE TABLE IF NOT EXISTS " . TABLE_DBIO_STATS . " (
+        dbio_stats_id int(11) NOT NULL auto_increment,
+        report_name varchar(255) NOT NULL default '',
+        action varchar(128) NOT NULL default '',
+        record_count int(11) NOT NULL default '0',
+        errors int(11) NOT NULL default '0',
+        warnings int(11) NOT NULL default '0',  
+        inserts int(11) NOT NULL default '0',  
+        updates int(11) NOT NULL default '0',
+        parse_time float NOT NULL default '0',
+        memory_usage int(11) NOT NULL default '0',
+        memory_peak_usage int(11) NOT NULL default '0',
+        date_added datetime NOT NULL default '0001-01-01 00:00:00',
+        PRIMARY KEY  (dbio_stats_id)
+    ) ENGINE=MyISAM";
+    $db->Execute ($sql);
+    if (!$sniffer->field_exists (TABLE_DBIO_STATS, 'report_name')) {
+        $db->Execute ("ALTER TABLE " . TABLE_DBIO_STATS . " ADD report_name varchar(255) NOT NULL default '' AFTER dbio_stats_id");
+    }
+
+    // -----
+    // Register the admin-level pages for use.
+    //
+    if (function_exists ('zen_page_key_exists')) {
+        if (!zen_page_key_exists ('toolsDbIo')) {
+            zen_register_admin_page ('toolsDbIo', 'BOX_TOOLS_DBIO', 'FILENAME_DBIO_MANAGER', '', 'tools', 'Y', init_dbio_next_sort ('tools'));
         }
-        
-        $db->Execute (
-            "INSERT INTO " . TABLE_DBIO_REPORTS . "
-                (handler_name, report_name, admin_id, last_updated_by, last_updated, field_info) 
-            VALUES
-                ('Products', 'meta_tags', 0, 0, now(), 0x5b2270726f64756374735f6964222c2270726f64756374735f6d6f64656c222c226d657461746167735f7469746c655f737461747573222c226d657461746167735f70726f64756374735f6e616d655f737461747573222c226d657461746167735f6d6f64656c5f737461747573222c226d657461746167735f70726963655f737461747573222c226d657461746167735f7469746c655f7461676c696e655f737461747573225d)"
-        );
-        $dbio_reports_id = $db->Insert_ID ();
-        foreach ($languages as $current_language) {
-            $current_language_id = $current_language['id'];
-            $db->Execute (
-                "INSERT INTO " . TABLE_DBIO_REPORTS_DESCRIPTION . "
-                    (dbio_reports_id, language_id, report_description) 
-                VALUES
-                    ($dbio_reports_id, $current_language_id, 'This template supports the export of a product''s meta-tags, including each product''s ID, model-number and meta-tag-related fields.')"
-            );
+        if (!zen_page_key_exists ('configDbIo')) {
+            zen_register_admin_page('configDbIo', 'BOX_CONFIGURATION_DBIO', 'FILENAME_CONFIGURATION', "gID=$cgi", 'configuration', 'Y', init_dbio_next_sort ('configuration'));
+        }
+        if (!zen_page_key_exists ('toolsDbIoCustomize')) {
+            zen_register_admin_page('toolsDbIoCustomize', 'BOX_TOOLS_DBIO_CUSTOMIZE', 'FILENAME_DBIO_CUSTOMIZE', '', 'tools', 'N', init_dbio_next_sort ('tools'));
         }
     }
-}
 
-// ----
-// Create the database tables for the I/O processing.
-//
-$sql = "CREATE TABLE IF NOT EXISTS " . TABLE_DBIO_STATS . " (
-    dbio_stats_id int(11) NOT NULL auto_increment,
-    report_name varchar(255) NOT NULL default '',
-    action varchar(128) NOT NULL default '',
-    record_count int(11) NOT NULL default '0',
-    errors int(11) NOT NULL default '0',
-    warnings int(11) NOT NULL default '0',  
-    inserts int(11) NOT NULL default '0',  
-    updates int(11) NOT NULL default '0',
-    parse_time float NOT NULL default '0',
-    memory_usage int(11) NOT NULL default '0',
-    memory_peak_usage int(11) NOT NULL default '0',
-    date_added datetime NOT NULL default '0001-01-01 00:00:00',
-    PRIMARY KEY  (dbio_stats_id)
-) ENGINE=MyISAM";
-$db->Execute ($sql);
-if (!$sniffer->field_exists (TABLE_DBIO_STATS, 'report_name')) {
-    $db->Execute ("ALTER TABLE " . TABLE_DBIO_STATS . " ADD report_name varchar(255) NOT NULL default '' AFTER dbio_stats_id");
-}
-
-// -----
-// Register the admin-level pages for use.
-//
-if (function_exists ('zen_page_key_exists')) {
-    if (!zen_page_key_exists ('toolsDbIo')) {
-        zen_register_admin_page ('toolsDbIo', 'BOX_TOOLS_DBIO', 'FILENAME_DBIO_MANAGER', '', 'tools', 'Y', init_dbio_next_sort ('tools'));
-    }
-    if (!zen_page_key_exists ('configDbIo')) {
-        zen_register_admin_page('configDbIo', 'BOX_CONFIGURATION_DBIO', 'FILENAME_CONFIGURATION', "gID=$cgi", 'configuration', 'Y', init_dbio_next_sort ('configuration'));
-    }
-    if (!zen_page_key_exists ('toolsDbIoCustomize')) {
-        zen_register_admin_page('toolsDbIoCustomize', 'BOX_TOOLS_DBIO_CUSTOMIZE', 'FILENAME_DBIO_CUSTOMIZE', '', 'tools', 'N', init_dbio_next_sort ('tools'));
-    }
+    // -----
+    // Now, update the current configuration version for the plugin.
+    //
+    $db->Execute ("UPDATE " . TABLE_CONFIGURATION . " SET configuration_value = '" . $version_release_date . "' WHERE configuration_key = 'DBIO_MODULE_VERSION' LIMIT 1");
 }
