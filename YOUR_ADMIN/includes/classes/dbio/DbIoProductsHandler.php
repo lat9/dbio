ddd53b6244e84899705e949c7243698fbdcaaf56
lat9 <lat9@vinosdefrutastropicales.com>
2017-04-19 09:57:34 -0400

    #100: Products import, manage dates
    Correct the date-handling for any product imports.

--- YOUR_ADMIN/includes/classes/dbio/DbIoProductsHandler.php
+++ YOUR_ADMIN/includes/classes/dbio/DbIoProductsHandler.php
@@ -574,5 +574,30 @@ class DbIoProductsHandler extends DbIoHandler
         }
         return parent::importUpdateRecordKey ($table_name, $table_fields, $products_id);
     }
+    
+    // -----
+    // Keep the product's date_added and last_updated fields in sync.  When a product is inserted, set its
+    // date_added, keeping its last_updated as the default; on an update, keep the existing date_added but
+    // make sure the product's last_updated date is "now".
+    //
+    protected function importBuildSqlQuery($table_name, $table_alias, $table_fields, $extra_where_clause = '', $is_override = false, $is_insert = true)
+    {
+        if ($table_name == TABLE_PRODUCTS) {
+            if (($is_override && $is_insert) || (!$is_override && $this->import_is_insert)) {
+                unset($table_fields['products_last_modified']);
+                $table_fields['products_date_added'] = array(
+                    'value' => 'now()',
+                    'type' => 'datetime'
+                );
+            } else {
+                unset($table_fields['products_date_added']);
+                $table_fields['products_last_modified'] = array(
+                    'value' => 'now()',
+                    'type' => 'datetime'
+                );
+            }
+        }
+        return parent::importBuildSqlQuery($table_name, $table_alias, $table_fields, $extra_where_clause, $is_override, $is_insert);
+    }
 
 }  //-END class DbIoProductsHandler
