98b7ebe5f382aa0706ebd36c7490e704c9b5ada8
lat9 <lat9@vinosdefrutastropicales.com>
2017-04-25 11:36:29 -0400

    #101: Update versions to 1.3.0
    Pre-release update.

--- YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
@@ -7,8 +7,8 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('DBIO_CURRENT_VERSION', '1.3.0-beta2');
-define('DBIO_CURRENT_UPDATE_DATE', '2017-04-xx');
+define('DBIO_CURRENT_VERSION', '1.3.0');
+define('DBIO_CURRENT_UPDATE_DATE', '2017-04-25');
 
 $version_release_date = DBIO_CURRENT_VERSION . ' (' . DBIO_CURRENT_UPDATE_DATE . ')';
 
