12e5d8f19f3cb4e93bba61aae8195a54d01deaf9
lat9 <lat9@vinosdefrutastropicales.com>
2017-10-04 09:08:05 -0400

    Update readme/version for 1.3.1 release
    As the title says ...

--- YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
@@ -7,8 +7,8 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('DBIO_CURRENT_VERSION', '1.3.0');
-define('DBIO_CURRENT_UPDATE_DATE', '2017-04-25');
+define('DBIO_CURRENT_VERSION', '1.3.1');
+define('DBIO_CURRENT_UPDATE_DATE', '2017-10-04');
 
 $version_release_date = DBIO_CURRENT_VERSION . ' (' . DBIO_CURRENT_UPDATE_DATE . ')';
 
