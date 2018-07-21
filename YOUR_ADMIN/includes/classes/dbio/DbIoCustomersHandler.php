78d1fc2c6298134d5c214347b93295788289ebac
lat9 <lat9@vinosdefrutastropicales.com>
2018-07-21 09:51:48 -0400

    #118: Add default address to `Customers` export

--- YOUR_ADMIN/includes/classes/dbio/DbIoCustomersHandler.php
+++ YOUR_ADMIN/includes/classes/dbio/DbIoCustomersHandler.php
@@ -12,11 +12,11 @@ if (!defined ('IS_ADMIN_FLAG')) {
 //
 class DbIoCustomersHandler extends DbIoHandler 
 {
-    public static function getHandlerInformation ()
+    public static function getHandlerInformation()
     {
-        DbIoHandler::loadHandlerMessageFile ('Customers'); 
+        DbIoHandler::loadHandlerMessageFile('Customers'); 
         return array (
-            'version' => '1.0.0',
+            'version' => '1.1.0',
             'handler_version' => '1.0.0',
             'include_header' => true,
             'export_only' => true,
@@ -32,22 +32,32 @@ class DbIoCustomersHandler extends DbIoHandler
     // This function, called during the overall class construction, is used to set this handler's database
     // configuration for the dbIO operations.
     //
-    protected function setHandlerConfiguration () 
+    protected function setHandlerConfiguration() 
     {
         $this->stats['report_name'] = 'Customers';
-        $this->config = self::getHandlerInformation ();
-        $this->config['keys'] = array (
-            TABLE_CUSTOMERS => array (
+        $this->config = self::getHandlerInformation();
+        $this->config['keys'] = array(
+            TABLE_CUSTOMERS => array(
                 'alias' => 'c',
-                'customers_id' => array (
+                'capture_key_value' => true,
+                'customers_id' => array(
                     'type' => self::DBIO_KEY_IS_VARIABLE | self::DBIO_KEY_SELECTED,
                 ),
             ),
         );
-        $this->config['tables'] = array (
-            TABLE_CUSTOMERS => array (
+        $this->config['tables'] = array(
+            TABLE_CUSTOMERS => array(
                 'alias' => 'c',
+                'join_clause' => 'INNER JOIN ' . TABLE_ADDRESS_BOOK . ' ab ON ab.address_book_id = c.customers_default_address_id',
             ),
+            TABLE_ADDRESS_BOOK => array ( 
+                'alias' => 'ab',
+                'no_from_clause' => true,
+                'io_field_overrides' => array (
+                    'address_book_id' => false,
+                    'customers_id' => false,
+                ),
+            ), 
         );
     }
 
--- YOUR_ADMIN/includes/languages/english/dbio/DbIoCustomersHandler.php
+++ YOUR_ADMIN/includes/languages/english/dbio/DbIoCustomersHandler.php
@@ -1,10 +1,10 @@
 <?php
 // -----
 // Part of the DataBase I/O Manager (aka DbIo) plugin, created by Cindy Merkin (cindy@vinosdefrutastropicales.com)
-// Copyright (c) 2016, Vinos de Frutas Tropicales.
+// Copyright (c) 2016-2018, Vinos de Frutas Tropicales.
 //
 
 // -----
 // Defines the handler's descriptive text.
 //
-define ('DBIO_CUSTOMERS_DESCRIPTION', 'This report-format supports the <em>export-only</em> of all fields within the &quot;customers&quot; table.');
+define ('DBIO_CUSTOMERS_DESCRIPTION', 'This report-format supports the <em>export-only</em> of all fields within the <code>customers</code> table as well as the customer\'s <em>default address</em> fields from the <code>address_book</code> table.');
