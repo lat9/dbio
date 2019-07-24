2ff9ab533619b8b05b3cd2602357c7de719b11cd
lat9 <lat9@vinosdefrutastropicales.com>
2019-07-24 10:52:38 -0400

    #138, #139, #140: Update readme/install version

--- YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
@@ -7,8 +7,8 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('DBIO_CURRENT_VERSION', '1.5.4');
-define('DBIO_CURRENT_UPDATE_DATE', '2019-04-30');
+define('DBIO_CURRENT_VERSION', '1.5.5-beta1');
+define('DBIO_CURRENT_UPDATE_DATE', '2019-07-24');
 
 $version_release_date = DBIO_CURRENT_VERSION . ' (' . DBIO_CURRENT_UPDATE_DATE . ')';
 
