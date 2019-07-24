db67c87ccf99288019c9af1bf4fe03ada0443875
lat9 <lat9@vinosdefrutastropicales.com>
2019-07-24 11:00:52 -0400

    Remove -beta1 designation

--- YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
@@ -7,7 +7,7 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('DBIO_CURRENT_VERSION', '1.5.5-beta1');
+define('DBIO_CURRENT_VERSION', '1.5.5');
 define('DBIO_CURRENT_UPDATE_DATE', '2019-07-24');
 
 $version_release_date = DBIO_CURRENT_VERSION . ' (' . DBIO_CURRENT_UPDATE_DATE . ')';
