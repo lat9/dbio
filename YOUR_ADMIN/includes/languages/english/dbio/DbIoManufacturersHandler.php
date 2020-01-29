615175b40b053e3fe5625e3eb973c375762393f7
lat9 <cindy@vinosdefrutastropicales.com>
2020-01-29 10:59:40 -0500

    #154: Enhance various handlers' on-screen instructions

--- YOUR_ADMIN/includes/languages/english/dbio/DbIoManufacturersHandler.php
+++ YOUR_ADMIN/includes/languages/english/dbio/DbIoManufacturersHandler.php
@@ -1,10 +1,10 @@
 <?php
 // -----
 // Part of the DataBase Import/Export (aka DbIo) plugin, created by Cindy Merkin (cindy@vinosdefrutastropicales.com)
-// Copyright (c) 2017, Vinos de Frutas Tropicales.
+// Copyright (c) 2017-2020, Vinos de Frutas Tropicales.
 //
 
 // -----
 // Defines the handler's descriptive text.
 //
-define ('DBIO_MANUFACTURERS_DESCRIPTION', 'This report-format supports import/export of <b>all</b> fields within the &quot;manufacturers&quot; and &quot;manufacturers_info&quot; tables');
+define('DBIO_MANUFACTURERS_DESCRIPTION', 'This report-format supports import/export of <b>all</b> fields within the <code>manufacturers</code> and <code>manufacturers_info</code> tables.<br /><br />If your store uses multiple languages, the <code>manufacturers_info</code> information will be exported for all of your store\'s languages in a single CSV record.');
--- YOUR_ADMIN/includes/languages/english/dbio/DbIoProductsAttribsRawHandler.php
+++ YOUR_ADMIN/includes/languages/english/dbio/DbIoProductsAttribsRawHandler.php
@@ -1,10 +1,10 @@
 <?php
 // -----
 // Part of the DataBase Import/Export (aka DbIo) plugin, created by Cindy Merkin (cindy@vinosdefrutastropicales.com)
-// Copyright (c) 2016-2019, Vinos de Frutas Tropicales.
+// Copyright (c) 2016-2020, Vinos de Frutas Tropicales.
 //
 
 // -----
 // Defines the handler's descriptive text.
 //
-define ('DBIO_PRODUCTSATTRIBSRAW_DESCRIPTION', 'This report-format supports import/export of <b>all</b> fields within the &quot;products_attributes&quot; and &quot;products_attributes_download&quot; tables, allowing you to add or change information about your store\'s product-attributes. Use the <code>v_dbio_command</code> column, set to <b>REMOVE</b> to remove an option from a product.<br /><br />One line in the CSV file corresponds to a single product-specific attribute.  All product-option and product-option-value entries must pre-exist in the database for a successful import of the attribute-related information.');
+define('DBIO_PRODUCTSATTRIBSRAW_DESCRIPTION', 'This report-format supports import/export of <b>all</b> fields within the <code>products_attributes</code> and <code>products_attributes_download</code> tables, allowing you to add or change information about your store\'s product-attributes. Use the <code>v_dbio_command</code> column, set to <b>REMOVE</b>, to remove an option/value pair from a product.<br /><br />One line in the CSV file corresponds to a single product-specific attribute.  All product-option and product-option-value entries must pre-exist in the database for a successful import of the attribute-related information.<br /><br />The export includes each option and option-value name, <em>in your store\'s default language</em>, to make the output a bit easier to read.  These values are not used for an import action.');
