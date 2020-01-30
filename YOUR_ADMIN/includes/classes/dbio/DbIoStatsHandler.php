9a78cc0503ff3241ffc5a92254dfc1fb47d6f9e2
lat9 <cindy@vinosdefrutastropicales.com>
2020-01-30 12:05:02 -0500

    #158: Refactoring prior to actual change.

--- YOUR_ADMIN/includes/classes/dbio/DbIoStatsHandler.php
+++ YOUR_ADMIN/includes/classes/dbio/DbIoStatsHandler.php
@@ -1,10 +1,10 @@
 <?php
 // -----
 // Part of the DataBase I/O Manager (aka DbIo) plugin, created by Cindy Merkin (cindy@vinosdefrutastropicales.com)
-// Copyright (c) 2016, Vinos de Frutas Tropicales.
+// Copyright (c) 2016-2020, Vinos de Frutas Tropicales.
 //
-if (!defined ('IS_ADMIN_FLAG')) {
-  exit ('Illegal access');
+if (!defined('IS_ADMIN_FLAG')) {
+  exit('Illegal access');
 }
 
 // -----
@@ -12,11 +12,11 @@ if (!defined ('IS_ADMIN_FLAG')) {
 //
 class DbIoStatsHandler extends DbIoHandler 
 {
-    public static function getHandlerInformation ()
+    public static function getHandlerInformation()
     {
-        DbIoHandler::loadHandlerMessageFile ('Stats'); 
-        return array (
-            'version' => '1.0.0',
+        DbIoHandler::loadHandlerMessageFile('Stats'); 
+        return array(
+            'version' => '1.6.0',
             'handler_version' => '1.0.0',
             'include_header' => true,
             'export_only' => true,
@@ -32,12 +32,12 @@ class DbIoStatsHandler extends DbIoHandler
     // This function, called during the overall class construction, is used to set this handler's database
     // configuration for the DbIo operations.
     //
-    protected function setHandlerConfiguration () 
+    protected function setHandlerConfiguration() 
     {
         $this->stats['report_name'] = 'Stats';
-        $this->config = self::getHandlerInformation ();
-        $this->config['tables'] = array (
-            TABLE_DBIO_STATS => array (
+        $this->config = self::getHandlerInformation();
+        $this->config['tables'] = array(
+            TABLE_DBIO_STATS => array(
                 'alias' => 'ds',
             ),
         );
--- YOUR_ADMIN/includes/languages/english/dbio/DbIoStatsHandler.php
+++ YOUR_ADMIN/includes/languages/english/dbio/DbIoStatsHandler.php
@@ -1,10 +1,10 @@
 <?php
 // -----
 // Part of the DataBase I/O Manager (aka DbIo) plugin, created by Cindy Merkin (cindy@vinosdefrutastropicales.com)
-// Copyright (c) 2016, Vinos de Frutas Tropicales.
+// Copyright (c) 2016-2020, Vinos de Frutas Tropicales.
 //
 
 // -----
 // Defines the handler's descriptive text.
 //
-define ('DBIO_STATS_DESCRIPTION', 'This report-format supports <em>export-only</em> of all fields within the &quot;dbio_stats&quot; table.');
+define('DBIO_STATS_DESCRIPTION', 'This report-format supports <em>export-only</em> of all fields within the &quot;dbio_stats&quot; table.');
