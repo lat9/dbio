a9a1e29599fff421702885c58ad5e5e2abfe0ca2
lat9 <cindy@vinosdefrutastropicales.com>
2020-02-10 07:52:36 -0500

    #161: Hide link-title on "View Import Details" pop-up

--- YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
@@ -14,8 +14,8 @@ if (empty($_SESSION['admin_id'])) {
     return;
 }
 
-define('DBIO_CURRENT_VERSION', '1.6.0');
-define('DBIO_CURRENT_UPDATE_DATE', '2020-02-08');
+define('DBIO_CURRENT_VERSION', '1.6.1-beta1');
+define('DBIO_CURRENT_UPDATE_DATE', '2020-02-10');
 
 $version_release_date = DBIO_CURRENT_VERSION . ' (' . DBIO_CURRENT_UPDATE_DATE . ')';
 
