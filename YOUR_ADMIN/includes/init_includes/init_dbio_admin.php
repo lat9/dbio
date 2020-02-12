704b4169ff74206f10711db0bf884123c9637df6
lat9 <cindy@vinosdefrutastropicales.com>
2020-02-12 13:14:19 -0500

    Update readme/init for 1.6.1 release

--- YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
@@ -14,7 +14,7 @@ if (empty($_SESSION['admin_id'])) {
     return;
 }
 
-define('DBIO_CURRENT_VERSION', '1.6.1-beta2');
+define('DBIO_CURRENT_VERSION', '1.6.1');
 define('DBIO_CURRENT_UPDATE_DATE', '2020-02-12');
 
 $version_release_date = DBIO_CURRENT_VERSION . ' (' . DBIO_CURRENT_UPDATE_DATE . ')';
