855b366cf4252f155137ec60bccfff31a6b3e7ef
lat9 <cindy@vinosdefrutastropicales.com>
2021-01-22 12:14:45 -0500

    Update init/readme for v1.6.5 release.

--- YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_dbio_admin.php
@@ -14,8 +14,8 @@ if (empty($_SESSION['admin_id'])) {
     return;
 }
 
-define('DBIO_CURRENT_VERSION', '1.6.5-beta2');
-define('DBIO_CURRENT_UPDATE_DATE', '2021-01-01');
+define('DBIO_CURRENT_VERSION', '1.6.5');
+define('DBIO_CURRENT_UPDATE_DATE', '2021-01-22');
 
 $version_release_date = DBIO_CURRENT_VERSION . ' (' . DBIO_CURRENT_UPDATE_DATE . ')';
 
