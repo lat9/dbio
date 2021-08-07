58a97ef08c7ebc59a1732cc798d856e9b2b77608
lat9 <cindy@vinosdefrutastropicales.com>
2021-08-07 08:59:11 -0400

    #192: Adding 'longtext' as a valid MySQL character type.

--- YOUR_ADMIN/includes/classes/dbio/DbIoHandler.php
+++ YOUR_ADMIN/includes/classes/dbio/DbIoHandler.php
@@ -13,7 +13,7 @@ abstract class DbIoHandler extends base
 //                                    C O N S T A N T S 
 // ----------------------------------------------------------------------------------
     // ----- Interface Constants -----
-    const DBIO_HANDLER_VERSION   = '1.6.5';
+    const DBIO_HANDLER_VERSION   = '1.6.7';
     // ----- Field-Import Status Values -----
     const DBIO_IMPORT_OK         = '--ok--';
     const DBIO_NO_IMPORT         = '--none--';
@@ -708,6 +708,7 @@ abstract class DbIoHandler extends base
                             case 'text':
                             case 'varchar':
                             case 'mediumtext':
+                            case 'longtext':
                                 $valid_string_field = true;  //-Indicate that a value string-type field was found and fall through to common processing
                             default:
                                 $field_type = 'string';
