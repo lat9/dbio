a85f5c741f7330d3884c95f1bd9a93a04a6b599c
lat9 <lat9@vinosdefrutastropicales.com>
2025-06-29 11:18:33 -0400

    #216: Add 'REMOVE' command to featured-products handler

--- YOUR_ADMIN/includes/classes/dbio/DbIoFeaturedHandler.php
+++ YOUR_ADMIN/includes/classes/dbio/DbIoFeaturedHandler.php
@@ -1,9 +1,9 @@
 <?php
 // -----
 // Part of the Database I/O Manager (aka DbIo) plugin, created by Cindy Merkin (cindy@vinosdefrutastropicales.com)
-// Copyright (c) 2015-2022, Vinos de Frutas Tropicales.
+// Copyright (c) 2015-2025, Vinos de Frutas Tropicales.
 //
-// Last updated: DbIo v2.0.0.
+// Last updated: DbIo v2.1.0
 //
 if (!defined('IS_ADMIN_FLAG')) {
     exit ('Illegal access');
@@ -18,7 +18,7 @@ class DbIoFeaturedHandler extends DbIoHandler
     {
         DbIoHandler::loadHandlerMessageFile('Featured'); 
         return [
-            'version' => '2.0.0',
+            'version' => '2.1.0',
             'handler_version' => '1.0.0',
             'include_header' => true,
             'export_only' => false,
@@ -26,24 +26,35 @@ class DbIoFeaturedHandler extends DbIoHandler
         ];
     }
 
-    public function exportInitialize ($language = 'all')
+    public function exportInitialize($language = 'all')
     {
-        $initialized = parent::exportInitialize ($language);
+        $initialized = parent::exportInitialize($language);
         if ($initialized === true) {
-            if ($this->where_clause != '') {
-                $this->where_clause .= ' AND ';
-            }
-            $this->where_clause .= "p.products_id = f.products_id";
             $this->order_by_clause .= 'f.products_id ASC';
         }
         return $initialized;
     }
 
-    public function exportPrepareFields (array $fields)
+    // -----
+    // For each 'featured' row's export, append the product's name, model-number and 'base price' to the output,
+    // adding those fields for reference use.
+    //
+    // Note: Since this handler supports DbIo commands, the base class' handling has appended an empty
+    // column as the last field to hold a potential command (this handler supports REMOVE).  Need to remove that
+    // column's data from the fields prior to inserting the 'helper' columns and then add it back.
+    //
+    public function exportPrepareFields(array $fields)
     {
-        $fields = parent::exportPrepareFields ($fields);
-        unset($fields['products_id'], $fields['featured_id']);
+        $fields = parent::exportPrepareFields($fields);
+        array_pop($fields);
+        unset($fields['featured_id']);
 
+        $products_id = $fields['products_id'];
+        
+        $fields['products_price'] = zen_get_products_base_price($products_id);
+        $fields['products_model'] = zen_get_products_model($products_id);
+        $fields['products_name'] = zen_get_products_name($products_id);
+        $fields['v_dbio_command'] = '';
         return $fields;
     }
 
@@ -59,38 +70,75 @@ class DbIoFeaturedHandler extends DbIoHandler
     {
         $this->stats['report_name'] = 'Featured';
         $this->config = self::getHandlerInformation();
+        $this->config['supports_dbio_commands'] = true;
         $this->config['keys'] = [
-            TABLE_PRODUCTS => [
-                'alias' => 'p',
-                'products_model' => [
-                    'type' => self::DBIO_KEY_IS_VARIABLE | self::DBIO_KEY_SELECTED,
-                ],
-            ],
             TABLE_FEATURED => [
                 'alias' => 'f',
                 'products_id' => [
-                    'type' => self::DBIO_KEY_IS_FIXED,
-                    'match_fixed_key' => 'p.products_id',
+                    'type' => self::DBIO_KEY_IS_VARIABLE | self::DBIO_KEY_SELECTED,
                 ],
             ],
         ];
         $this->config['tables'] = [
-            TABLE_PRODUCTS => [
-                'alias' => 'p',
-                'key_fields_only' => true,
-            ],
             TABLE_FEATURED => [
                 'alias' => 'f',
                 'io_field_overrides' => [
-                    'products_id' => false,
                     'featured_id' => 'no-header',
                 ],
-            ],
+            ], 
+        ];
+        $this->config['additional_headers'] = [
+            'v_products_price' => self::DBIO_FLAG_NONE,
+            'v_products_model' => self::DBIO_FLAG_NONE,
+            'v_products_name' => self::DBIO_FLAG_NONE,
         ];
     }
 
-    protected function importAddField ($table_name, $field_name, $field_value) {
-        $this->debugMessage ("Featured::importAddField ($table_name, $field_name, $field_value)");
+    // -----
+    // This function, called for header-element being imported, can return one of three values:
+    //
+    // - DBIO_IMPORT_OK ........ The field has no special-handling requirements.
+    // - DBIO_NO_IMPORT ........ The field's value should not set directly to the database for the import; implying
+    //                           that the field is calculated separately by the handler's processing.
+    // - DBIO_SPECIAL_IMPORT ... The field requires special-handling by the handler to create the associated database elements.
+    //
+    protected function importHeaderFieldCheck($field_name)
+    {
+        if ($field_name !== 'featured_id' && in_array($field_name, array_keys($this->tables['featured']['fields']))) {
+            return self::DBIO_IMPORT_OK;
+        }
+        return self::DBIO_NO_IMPORT;
+    }
+
+    protected function importProcessField($table_name, $field_name, $language_id, $field_value)
+    {
+        global $db;
+
+        $this->debugMessage("Featured::importProcessField($table_name, $field_name, $language_id, $field_value)");
+
+        // -----
+        // The 'products_id' supplied must be associated with a defined product.
+        //
+        if ($field_name === 'products_id') {
+            $check = $db->Execute(
+                "SELECT products_id
+                   FROM " . TABLE_PRODUCTS . "
+                  WHERE products_id = " . (int)$field_value . "
+                  LIMIT 1"
+            );
+            if ($check->EOF) {
+                $this->debugMessage("[*] $table_name.$field_name, line #" . $this->stats['record_count'] . ": Value ($field_value) is a not valid 'products_id'.", self::DBIO_ERROR);
+                $this->record_status = false;
+                return;
+            }
+        }
+
+        parent::importProcessField($table_name, $field_name, $language_id, $field_value);
+    }
+
+    protected function importAddField($table_name, $field_name, $field_value)
+    {
+        $this->debugMessage("Featured::importAddField($table_name, $field_name, $field_value)");
         switch ($table_name) {
             case TABLE_FEATURED:
                 if ($this->import_is_insert === true) {
@@ -99,16 +147,43 @@ class DbIoFeaturedHandler extends DbIoHandler
                     } elseif ($field_name === 'featured_last_modified') {
                         $field_value = self::DBIO_NO_IMPORT;
                     }
-                } else {
-                    if ($field_name === 'featured_last_modified') {
-                        $field_value = 'now()';
-                    }
+                } elseif ($field_name === 'featured_last_modified') {
+                    $field_value = 'now()';
                 }
-                parent::importAddField ($table_name, $field_name, $field_value);
+                parent::importAddField($table_name, $field_name, $field_value);
                 break;
 
             default:
                 break;
-        }  //-END switch interrogating $table_name
-    }  //-END function importAddField
-}  //-END class DbIoFeaturedHandler
+        }
+    }
+
+    // -----
+    // This function, called by the base DbIoHandler class when a non-blank v_dbio_command field is found in the
+    // current import-record, gives this handler a chance to REMOVE a featured product's record from the database.
+    //
+    protected function importHandleDbIoCommand($command, $data)
+    {
+        global $db;
+
+        $command = dbio_strtoupper($command);
+        if ($command !== self::DBIO_COMMAND_REMOVE) {
+            $this->debugMessage("Unrecognized command ($command) found at line #" . $this->stats['record_count'] . "; the operation was not performed.", self::DBIO_ERROR);
+        } else {
+            $products_id = $this->importGetFieldValue('products_id', $data);
+            if ($products_id === false) {
+                $this->debugMessage("Missing 'v_products_id' column at line #" . $this->stats['record_count'] . "; featured product not removed.", self::DBIO_ERROR);
+            } else {
+                $this->debugMessage("Removing featured-product entry for product_id #" . $products_id, self::DBIO_STATUS);
+                if ($this->operation !== 'check') {
+                    $db->Execute(
+                        "DELETE FROM " . TABLE_FEATURED . "
+                          WHERE products_id = " . (int)$products_id . "
+                          LIMIT 1"
+                    );
+                }
+            }
+        }
+        return false;
+    }
+}
--- YOUR_ADMIN/includes/languages/english/dbio/DbIoFeaturedHandler.php
+++ YOUR_ADMIN/includes/languages/english/dbio/DbIoFeaturedHandler.php
@@ -1,10 +1,10 @@
 <?php
 // -----
 // Part of the DataBase Import/Export (aka DbIo) plugin, created by Cindy Merkin (cindy@vinosdefrutastropicales.com)
-// Copyright (c) 2015-2016, Vinos de Frutas Tropicales.
+// Copyright (c) 2015-2025, Vinos de Frutas Tropicales.
 //
 
 // -----
 // Defines the handler's descriptive text.
 //
-define ('DBIO_FEATURED_DESCRIPTION', 'This report-format supports import/export of all fields within the &quot;featured&quot; table, the <em>Featured Products</em> information.');
+define ('DBIO_FEATURED_DESCRIPTION', 'This report-format supports import/export of all fields within the &quot;featured&quot; table, the <em>Featured Products</em> information.<br><br><b>Notes</b><ol><li>For an import to be successful, at a minimum the <code>v_products_id</code> column must be present.</li><li>The <code>v_products_id</code> supplied on an import must be associated with a valid product.</li><li>A product can be removed as a featured product by including a <code>v_dbio_command</code> column with <b>REMOVE</b> for a specific products_id.</li></ol>');
